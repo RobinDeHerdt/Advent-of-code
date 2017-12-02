5c57bfcbb19d7e16240ef6d44a4cd0ec5ce36766
Robin De Herdt <robindh95@gmail.com>
2017-12-02 14:22:08 +0100

    Added documentation

--- 01/inverse-captcha.py
+++ 01/inverse-captcha.py
@@ -1,17 +1,23 @@
-# Input digits
+# Get puzzle digits
 digits = str(input("What's the puzzle input? \n"))
 
-digitsum = 0
-digitcount = len(digits)
+solution = 0
+numberofdigits = len(digits)
 
+# Loop over each digit
 for index, digit in enumerate(digits):
-    # Check if the next digit is the same as the current.
-    if index + 1 < digitcount:
+
+    # Get the next index in the array.
+    # When the index is out of array bounds, set it to 0.
+    if index + 1 < numberofdigits:
         nextindex = index + 1
     else:
         nextindex = 0
 
+    # Check if the current digit is the same as the next.
+    # If it is, add it to the solution.
     if int(digit) == int(digits[nextindex]):
-        digitsum += int(digit)
+        solution += int(digit)
 
-print digitsum
+# Print the solution
+print solution
