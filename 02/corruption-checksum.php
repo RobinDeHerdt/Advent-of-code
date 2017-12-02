52229b1123b70ded6752ff8f9b87be2a5166ee25
Robin De Herdt <robindh95@gmail.com>
2017-12-02 15:09:46 +0100

    Added documentation

--- 02/corruption-checksum.php
+++ 02/corruption-checksum.php
@@ -1,6 +1,6 @@
 <?php
 
-$dataset = file_get_contents('datasets/dataset');
+$dataset = file_get_contents('spreadsheets/spreadsheet');
 
 # Explode on EOL (End Of Line constant)
 $dataset_rows = explode(PHP_EOL, $dataset);
@@ -8,17 +8,19 @@ $dataset_rows = explode(PHP_EOL, $dataset);
  # Define an array for all the outcomes.
 $row_outcomes = [];
 
+# Loop over each row (string).
 foreach ($dataset_rows as $dataset_row) {
   # Remove excess whitespace from within the string.
   $dataset_clean_row = preg_replace('/\s+/', ' ', $dataset_row );
 
-  # Explode on space
+  # Create an array of numbers from the current row.
   $row_numbers = explode(" ", $dataset_clean_row);
 
   # Set the initial values to the first number in the array.
   $highest_number = $row_numbers[0];
   $lowest_number = $row_numbers[0];
 
+  # Loop over each row and get both the highest and the lowest number.
   foreach ($row_numbers as $row_number) {
     if ($row_number > $highest_number) {
       $highest_number = $row_number;
@@ -29,10 +31,10 @@ foreach ($dataset_rows as $dataset_row) {
     }
   }
 
-  # Calculate the difference between the highest and the lowest value.
+  # Calculate the difference between the highest and the lowest number.
   $diff =  (int)$highest_number - (int)$lowest_number;
 
-  # When done looping, push the outcome to the array.
+  # Add the outcome to the outcomes array.
   $row_outcomes[] = $diff;
 }
 
@@ -43,4 +45,5 @@ foreach ($row_outcomes as $row_outcome) {
   $checksum += $row_outcome;
 }
 
+# Print the result.
 echo "Checksum: " . $checksum . "\n";
