225751a6500c26fd18bd23f88c4254a88054baa3
Robin De Herdt <robindh95@gmail.com>
2017-12-03 19:22:18 +0100

    Added working code snippet for 'spiral memory'

--- 03/spiral-memory.php
+++ 03/spiral-memory.php
@@ -3,23 +3,46 @@
 $number = readline("Gimme that puzzle input \n");
 
 $peel = 0;
-$spiral_width = 1;
-while (pow($spiral_width, 2) < $number) {
-  $spiral_width += 2;
+$peel_width = 1;
+while (pow($peel_width, 2) < $number) {
+  $peel_width += 2;
   $peel += 1;
 }
 
 $peel_number_count = $peel * 8;
-$spiral_number_count = pow($spiral_width, 2);
+$spiral_number_count = pow($peel_width, 2);
 
 # Lower bound is not part of the current peel. Keep that in mind.
 $peel_lower_bound = $spiral_number_count - $peel_number_count;
 $peel_upper_bound = $spiral_number_count;
 $peel_position = $number - $peel_lower_bound;
 
-echo "Spiral width: " . $spiral_width . "\n";
+$peel_centers = [];
+for ($i = 1; $i <= 4; $i++) {
+  $distance_to_center = ($peel_width - 1) / 2;
+  $peel_centers[] = (($peel_width - 1) * $i) - $distance_to_center;
+}
+
+$shortest_distance = $peel_width;
+foreach ($peel_centers as $peel_center) {
+  $distance_to_center = abs($peel_center - $peel_position);
+  if ($distance_to_center < $shortest_distance) {
+    $shortest_distance = $distance_to_center;
+  }
+}
+
+$total_shortest_distance = $shortest_distance + $peel;
+
+echo "Spiral width: " . $peel_width . "\n";
 echo "Spiral peel: " . $peel . "\n";
 echo "Number count on this peel: " . $peel_number_count . "\n";
 echo "Peel lower bound: " . $peel_lower_bound . "\n";
 echo "Peel upper bound: " . $peel_upper_bound . "\n";
 echo "Peel position: " . $peel_position . "\n";
+echo "--------- \n";
+foreach ($peel_centers as $peel_center) {
+  echo $peel_center . "\n";
+}
+echo "--------- \n";
+echo "Steps from middle: " . $shortest_distance . "\n";
+echo "Total steps: " . $total_shortest_distance . "\n";
